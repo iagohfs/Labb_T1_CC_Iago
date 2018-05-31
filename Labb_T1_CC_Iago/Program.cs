254e6f5603237c9b17769d4aea31bf1ab1d65238
iagohfs <iago_henriquefs@hotmail.com>
2018-05-31 15:41:41 +0200

    Add project files.

---
+++ Labb_T1_CC_Iago/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb_T1_CC_Iago
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
